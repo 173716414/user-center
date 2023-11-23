67a27d827051e577d829cb88d5131cf66f9092b4
Victor <47047497+173716414@users.noreply.github.com>
2023-11-23 09:43:50 +0800

    Initial commit

---
+++ src/main/java/com/yupi/usercenter/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.yupi.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.usercenter.model.User;
+
+/**
+ * @Author：Victor_htq
+ * @Package：com.yupi.usercenter.mapper
+ * @Project：user-center
+ * @name：UserMapper
+ * @Date：2023/11/22 15:48
+ * @Filename：UserMapper
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
